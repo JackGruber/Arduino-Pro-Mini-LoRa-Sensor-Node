6aeec4f1a02f8374f161bd8792094b086c9dde36
JackGruber <24863925+JackGruber@users.noreply.github.com>
2019-10-26 11:39:02 +0200

    Call Lora and Setup Pins

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <Arduino.h>
 #include "io_pins.h"
+#include "functions.h"
+#include "lorawan.h"
 
 void setup() {
   Serial.begin(115200);
   Serial.println(F("Starting DHT ..."));
+  Setup_Pins();
+  LoRaWANSetup();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  LoraWANDo();
 }

c518c5ed099420f5aaac628abb727ae8f83e7450
JackGruber <24863925+JackGruber@users.noreply.github.com>
2019-10-30 16:25:30 +0100

    BUG: sizeof

--- src/lorawan.cpp
+++ src/lorawan.cpp
@@ -110,7 +110,7 @@ void LoraWANDo_send(osjob_t* j)
         Serial.println(F("OP_TXRXPEND, not sending"));
     } else {
         // Prepare upstream data transmission at the next possible time.
-        LMIC_setTxData2(1, LORA_DATA, sizeof(LORA_DATA)-1, 0);
+        LMIC_setTxData2(1, LORA_DATA, sizeof(LORA_DATA), 0);
         Serial.println(F("Packet queued"));
     }
     // Next TX is scheduled after TX_COMPLETE event.

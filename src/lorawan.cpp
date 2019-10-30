e913cb90d0bad88872d6b76a6a510a5f5ba82feb
JackGruber <24863925+JackGruber@users.noreply.github.com>
2019-10-30 22:37:53 +0100

    Vhange to OTAA setup

--- src/lorawan.cpp
+++ src/lorawan.cpp
@@ -20,81 +20,20 @@ static uint8_t LORA_DATA[4];
 // Schedule TX every this many seconds (might become longer due to duty cycle limitations).
 const unsigned TX_INTERVAL = LORA_TX_INTERVAL;
 
-// These callbacks are only used in over-the-air activation, so they are
-// left empty here (we cannot leave them out completely unless
-// DISABLE_JOIN is set in config.h, otherwise the linker will complain).
-void os_getArtEui (u1_t* buf) { }
-void os_getDevEui (u1_t* buf) { }
-void os_getDevKey (u1_t* buf) { }
+void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
+void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
+void os_getDevKey (u1_t* buf) { memcpy_P(buf, APPKEY, 16);}
 
 bool GO_DEEP_SLEEP = false;
-
 
 void LoRaWANSetup()
 {
     Serial.println(F("LoRaWAN_Setup ..."));
-    delay(100);
-    
+
     // LMIC init
     os_init();
-
     // Reset the MAC state. Session and pending data transfers will be discarded.
     LMIC_reset();
-
-    // Set static session parameters. Instead of dynamically establishing a session
-    // by joining the network, precomputed session parameters are be provided.
-    #ifdef PROGMEM
-        // On AVR, these values are stored in flash and only copied to RAM
-        // once. Copy them to a temporary buffer here, LMIC_setSession will
-        // copy them into a buffer of its own again.
-        uint8_t appskey[sizeof(APPSKEY)];
-        uint8_t nwkskey[sizeof(NWKSKEY)];
-        memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
-        memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
-        LMIC_setSession (0x13, DEVADDR, nwkskey, appskey);
-    #else
-        // If not running an AVR with PROGMEM, just use the arrays direc
-        LMIC_setSession (0x13, DEVADDR, NWKSKEY, APPSKEY);
-    #endif
-
-    #if defined(CFG_eu868)
-        // Set up the channels used by the Things Network, which corresponds
-        // to the defaults of most gateways. Without this, only three base
-        // channels from the LoRaWAN specification are used, which certainly
-        // works, so it is good for debugging, but can overload those
-        // frequencies, so be sure to configure the full frequency range of
-        // your network here (unless your network autoconfigures them).
-        // Setting up channels should happen after LMIC_setSession, as that
-        // configures the minimal channel set.
-        LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band 
-        LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
-        LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-        LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-        LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-        LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-        LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-        LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-        LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
-        // TTN defines an additional channel at 869.525Mhz using SF9 for class B
-        // devices' ping slots. LMIC does not have an easy way to define set this
-        // frequency and support for class B is spotty and untested, so this
-        // frequency is not configured here.
-    #elif defined(CFG_us915)
-        // NA-US channels 0-71 are configured automatically
-        // but only one group of 8 should (a subband) should be active
-        // TTN recommends the second sub band, 1 in a zero based count.
-        // https://github.com/TheThingsNetwork/gateway-conf/blob/master/US-global_conf.json
-        LMIC_selectSubBand(1);
-    #endif
-
-    // Disable link check validation
-    LMIC_setLinkCheckMode(0);
-
-    // TTN uses SF9 for its RX2 window.
-    LMIC.dn2Dr = DR_SF9;
-
-    // Set data rate and transmit power for uplink (note: txpow seems to be ignored by the library)
-    LMIC_setDrTxpow(LMIC_LORA_SF,14);
 
     // Start job
     LoraWANDo_send(&sendjob);

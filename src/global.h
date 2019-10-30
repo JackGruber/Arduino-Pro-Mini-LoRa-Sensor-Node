781d0a76e6a4890286b94786d56f1be02dd2794e
JackGruber <24863925+JackGruber@users.noreply.github.com>
2019-10-30 16:10:01 +0100

    Add function ReadDHTSensor

--- src/global.h
+++ src/global.h
@@ -3,9 +3,9 @@
 #define _GLOBAL_H
 
 #include <Arduino.h>
-#include <DHT.h>
 
-extern DHT DHTSENSOR;
 extern volatile int POWER_DOWN_SLEEP_COUNTER;
+extern float HUMIDITY;
+extern float TEMPERATURE;
 
 #endif
